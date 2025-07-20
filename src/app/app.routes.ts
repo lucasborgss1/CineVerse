7af3d0119771837906496362d2d1c040e7ce0667
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-20 20:00:11 -0300

    ajustando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,30 @@
 import { Routes } from '@angular/router';
 import { PaginaInicial } from './pages/pagina-inicial/pagina-inicial';
+import { PaginaMovies } from './pages/pagina-movies/pagina-movies';
+import { PaginaSeries } from './pages/pagina-series/pagina-series';
+import { PaginaAnimated } from './pages/pagina-animated/pagina-animated';
+import { PaginaDocumentary } from './pages/pagina-documentary/pagina-documentary';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'CineVerse', pathMatch: 'full' }, // pathMatch é necessário
+  { path: '', redirectTo: 'cineverse', pathMatch: 'full' },
   {
-    path: 'CineVerse',
+    path: 'cineverse',
     component: PaginaInicial,
+  },
+  {
+    path: 'movies',
+    component: PaginaMovies,
+  },
+  {
+    path: 'series',
+    component: PaginaSeries,
+  },
+  {
+    path: 'desenhos',
+    component: PaginaAnimated,
+  },
+  {
+    path: 'docs',
+    component: PaginaDocumentary,
   },
 ];
