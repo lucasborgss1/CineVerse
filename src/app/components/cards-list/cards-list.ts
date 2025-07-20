bd9951f8df2ee12e2995ef69282e9c282ac60aa0
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-20 18:48:00 -0300

    adicionando componente cards-list

---
+++ src/app/components/cards-list/cards-list.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MediaItem } from '../../models/models';
+import { CommonModule } from '@angular/common';
+import { ContentCards } from '../content-cards/content-cards';
+import { CarouselModule } from 'primeng/carousel';
+
+@Component({
+  selector: 'app-cards-list',
+  standalone: true,
+  imports: [CommonModule, ContentCards, CarouselModule],
+  templateUrl: './cards-list.html',
+  styleUrl: './cards-list.css',
+})
+export class CardsList {
+  @Input() list: MediaItem[] = [];
+  @Output() cardSelected = new EventEmitter<any>();
+
+  onCardClick(item: any) {
+    this.cardSelected.emit(item);
+  }
+
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 7,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '991px',
+      numVisible: 6,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 5,
+      numScroll: 1,
+    },
+  ];
+}
