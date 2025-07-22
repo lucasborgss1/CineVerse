01194d5451b6bbb12be82015e482131bf502f113
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-22 19:41:10 -0300

    adicionando conteúdo da página documentarios

--- src/app/pages/pagina-documentary/pagina-documentary.ts
+++ src/app/pages/pagina-documentary/pagina-documentary.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Header } from '../../components/header/header';
+import { MovieService } from '../../services/movie-service';
+import { Footer } from '../../components/footer/footer';
+import { MediaItem } from '../../models/models';
+import { CommonModule } from '@angular/common';
+import { NgbdCarouselPause } from '../../components/ngbd-carousel-pause/ngbd-carousel-pause';
+import { CardsList } from '../../components/cards-list/cards-list';
+import { MovieDetails } from '../../components/movie-details/movie-details';
 
 @Component({
   selector: 'app-pagina-documentary',
-  imports: [],
+  imports: [
+    Header,
+    Footer,
+    CommonModule,
+    NgbdCarouselPause,
+    CardsList,
+    MovieDetails,
+  ],
   templateUrl: './pagina-documentary.html',
-  styleUrl: './pagina-documentary.css'
+  styleUrl: './pagina-documentary.css',
 })
 export class PaginaDocumentary {
+  moviesDocumentary: MediaItem[] = [];
+  seriesDocumentary: MediaItem[] = [];
 
+  carouselData: MediaItem[] = [];
+
+  selectedMedia: any = null;
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.movieService.getDataByGenre('movie', 99).subscribe((response) => {
+      for (let index = 0; index < 5; index++) {
+        this.carouselData.push(response.results[index]);
+      }
+      this.moviesDocumentary = response.results;
+    });
+
+    this.movieService.getDataByGenre('tv', 99).subscribe((response) => {
+      this.seriesDocumentary = response.results;
+    });
+  }
+
+  openDetails(media: MediaItem) {
+    this.selectedMedia = media;
+  }
 }
