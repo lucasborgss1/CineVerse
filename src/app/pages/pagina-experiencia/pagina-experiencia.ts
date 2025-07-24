62f04b52e1f901edcfe960c45ed6adaee2372f47
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-24 11:37:52 -0300

    adicionando carrosel na pagina experiencia

--- src/app/pages/pagina-experiencia/pagina-experiencia.ts
+++ src/app/pages/pagina-experiencia/pagina-experiencia.ts
@@ -5,15 +5,23 @@ import { Footer } from '../../components/footer/footer';
 import { CommonModule } from '@angular/common';
 import { HeaderExperiencia } from '../../components/header-experiencia/header-experiencia';
 import { Background } from '../../components/background/background';
+import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pagina-experiencia',
-  imports: [Footer, CommonModule, HeaderExperiencia, Background],
+  imports: [
+    Footer,
+    CommonModule,
+    HeaderExperiencia,
+    Background,
+    NgbCarouselModule,
+  ],
   templateUrl: './pagina-experiencia.html',
   styleUrl: './pagina-experiencia.css',
 })
 export class PaginaExperiencia implements OnInit {
   selectedMedia: any = null;
+  images = ['/images/img1.png', '/images/img2.png', '/images/img3.png'];
 
   constructor(private router: Router) {}
 
